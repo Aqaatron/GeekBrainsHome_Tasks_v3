4a7238f4c03671fc566465499b009d4600cc13a3
Андрей Cидоров <aqaatron@gmail.com>
2021-03-15 19:16:44 +0300

    prac5

---
+++ prac5/prac5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prac5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
