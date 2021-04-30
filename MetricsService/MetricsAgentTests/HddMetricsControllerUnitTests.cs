a3f96469ebb7b15ef7769e1b5e3e53dbb2d53b65
Андрей Cидоров <aqaatron@gmail.com>
2021-04-30 14:54:19 +0300

    MetricsService prac #1

---
+++ MetricsService/MetricsAgentTests/HddMetricsControllerUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using MetricsAgent;
+using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MetricsManager.Enums;
+
+namespace MetricsAgentTests
+{
+    public class HddMetricsControllerUnitTests
+    {
+        private HddMetricsController controller;
+
+        public HddMetricsControllerUnitTests()
+        {
+            controller = new HddMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetMetrics();
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+    }
+}
---
+++ MetricsService/MetricsAgentTests/RamMetricsControllerUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using MetricsAgent;
+using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MetricsManager.Enums;
+
+namespace MetricsAgentTests
+{
+    public class RamMetricsControllerUnitTests
+    {
+        private RamMetricsController controller;
+
+        public RamMetricsControllerUnitTests()
+        {
+            controller = new RamMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+
+
+            //Act
+            var result = controller.GetMetrics();
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
